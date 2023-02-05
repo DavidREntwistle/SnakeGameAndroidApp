5fbf6070bfa48d0a605d2fc61bb9977e121d79aa
David Entwistle <davidautodev@gmail.com>
2023-02-05 20:51:08 +0000

    Implementing the start of the Snake Game

---
+++ SnakeGame/app/src/main/java/com/davidrentwistle/snakegame/SnakeGame.kt
@@ -0,0 +1,18 @@
+package com.davidrentwistle.snakegame
+
+class SnakeGame {
+
+    // initialize game variables and settings here
+
+    fun start() {
+        // start the game logic here
+    }
+
+    fun update() {
+        // update the game state here
+    }
+
+    fun stop() {
+        // stop the game logic here
+    }
+}
